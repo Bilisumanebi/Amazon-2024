e2a05786deba8c95d54c7058cddca7e9be86b1d3
Bilisumanebi <hbilisuma@yahoo.com>
2024-11-22 22:02:48 -0600

    product

--- Amazon-Clone/src/App.jsx
+++ Amazon-Clone/src/App.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
-import { useState } from 'react'
-import './App.css'
+// import { useState } from 'react'
+// import './App.css'
 import Header from './Components/Header/Header'
 import Carousel from './Components/Carousel/CarouselEffect'
 import Catagory from './Components/Category/Catagory'
+import Product from './Components/Product/Product'
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
     <Header />
     <Carousel />
     <Catagory />
+    <Product />
  
   
     
---
+++ Amazon-Clone/src/Components/Product/Product.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Productcard from './Productcard'
+import classes from './product.module.css'
+
+const Product = () => {
+      const [products, setProducts] = useState ([])
+      
+      useEffect(()=> {
+            axios.get("https://fakestoreapi.com/products")
+            .then((res)=>{
+                  // console.log(res);
+                  setProducts(res.data)               
+            })
+            .catch((err)=> {
+                  console.log(err);
+                  
+            })
+
+      }, [])
+   
+      
+      
+      return (
+            <section className={classes.products_container}>
+                  {products.length===0? (
+                        <p>Loading...</p>
+                  ):(
+                  
+                        products.map((singleProduct)=> (                           
+                              
+                              <Productcard product={singleProduct} key={singleProduct.id}/>
+                        ))
+)}
+
+
+            </section>
+      )
+}
+
+export default Product
